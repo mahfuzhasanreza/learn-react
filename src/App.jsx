56fd71c963fd582d4a5952769e8d226689e32eb1
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-10-18 00:05:36 +0600

    added Dynamic Content To JSX

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,27 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Student></Student>
     </>
+  )
+}
+
+function Person() {
+  const age = 20;
+  return <h3>My name is Mahfuz. My age is {age}</h3>
+}
+
+function Student() {
+  return (
+    <div>
+      <h3>This is a student</h3>
+      <p>Name:</p>
+      <p>Age:</p>
+    </div>
   )
 }
 
