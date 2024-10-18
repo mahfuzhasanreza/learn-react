fb9646b012c544682766337644d08edc99a61f64
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-10-18 11:29:42 +0600

    added Conditional Rendering: If, Ternary And AND OR

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -7,10 +7,33 @@
 // }
 
 
-// conditional rendering
+// conditional rendering - option 1
+// export default function Todo({ task, isDone }) {
+//     if (isDone) {
+//         return <li>Finished: {task}</li>
+//     }
+//     return <li>Work-on: {task}</li>
+// }
+
+
+// conditional rendering using ternary operator - option 2
+// export default function Todo({ task, isDone }) {
+//     return (
+//         <li>{isDone ? 'Finished' : 'Work-on'}: {task}</li>
+//     )
+// }
+
+
+// // conditional rendering - option 3 : &&
+// export default function Todo({ task, isDone }) {
+//     return (
+//         <li>{task} {isDone && ': Done'} </li>
+//     )
+// }
+
+// conditional rendering - option 4 : ||
 export default function Todo({ task, isDone }) {
-    if (isDone) {
-        return <li>Finished: {task}</li>
-    }
-    return <li>Work-on: {task}</li>
+    return (
+        <li>{task} {isDone || ': Pending'} </li>
+    )
 }
