ffc34c511016db2926915f44739b2ce9b2f93b7e
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-10-30 11:41:06 +0600

    Create And Load Fake Data, Chatgpt Json, Online Api

--- es6/src/components/Watch/Watch.jsx
+++ es6/src/components/Watch/Watch.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const Watch = () => {
+const Watch = ({ watch }) => {
+    const { name, price } = watch;
+
     return (
         <div>
-            
+            <h2>Watch: {name}</h2>
+            <p>Price: {price}</p>
         </div>
     );
 };
