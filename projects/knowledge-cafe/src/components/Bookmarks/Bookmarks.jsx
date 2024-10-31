25a1b8abf996c8f53a8903db8f76d95a069c36d1
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-10-31 21:48:56 +0600

    bookmark section reading time text-center

--- projects/knowledge-cafe/src/components/Bookmarks/Bookmarks.jsx
+++ projects/knowledge-cafe/src/components/Bookmarks/Bookmarks.jsx
@@ -5,7 +5,7 @@ const Bookmarks = ({ bookmarks, readingTime }) => {
     return (
         <div className="md:w-1/3 bg-gray-300 ml-5 mt-3 rounded-2xl pt-5">
             <div>
-                <h3 className="text-3xl">Reading Time: {readingTime}</h3>
+                <h3 className="text-3xl text-center">Reading Time: {readingTime}</h3>
             </div>
             <h2 className="text-3xl text-center">Bookmarked Blogs: {bookmarks.length}</h2>
             {
