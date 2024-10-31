e745301f6e880378f8866136fffcaf485738bf45
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-10-31 11:32:44 +0600

    Local storage to store data just one item

---
+++ projects/memorable-water-bottle/src/utilities/localstorage.js
@@ -0,0 +1,21 @@
+const getStoreCart = () => {
+    const storedCartString = localStorage.getItem('cart');
+    if (storedCartString) {
+        return JSON.parse(storedCartString);
+    }
+    return [];
+}
+
+const saveCartToLS = cart => {
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringified);
+}
+
+const addToLS = id => {
+    const cart = getStoreCart();
+    cart.push(id);
+    // save to local storage 
+    saveCartToLS(cart);
+}
+
+export { addToLS }
