bbbd7b92f30cc3f1203621121366ea77a18bd317
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-11-02 14:33:43 +0600

    Reusable form using props and children

--- projects/form-master/src/App.jsx
+++ projects/form-master/src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
-import HookForm from './components/HookForm/HookForm'
+import ReusableForm from './components/ReusableForm/ReusableForm'
+// import HookForm from './components/HookForm/HookForm'
 // import RefForm from './components/RefForm/RefForm'
 // import SimpleForm from './components/SimpleForm/SimpleForm'
 // import StatefulForm from './components/StatefulForm/StatefulForm'
 
 function App() {
+
+  const handleSignUpSubmit = data => {
+    console.log('sign up data:', data);
+  }
+
+  const handleUpdateProfile = data => {
+    console.log('update profile data:', data);
+  }
 
   return (
     <>
@@ -12,7 +21,21 @@ function App() {
       {/* <SimpleForm></SimpleForm> */}
       {/* <StatefulForm></StatefulForm> */}
       {/* <RefForm></RefForm> */}
-      <HookForm></HookForm>
+      {/* <HookForm></HookForm> */}
+      <ReusableForm
+        formTitle={'Sign Up'}
+        handleSubmit={handleSignUpSubmit}>
+        <div>
+          <h2>Sign Up</h2>
+          <p>Please sign up</p>
+        </div>
+      </ReusableForm>
+      <ReusableForm formTitle={'Profile Update'} handleSubmit={handleUpdateProfile} submitBtnText='Update'>
+        <div>
+          <h2>Update Profile</h2>
+          <p>Please update</p>
+        </div>
+      </ReusableForm>
     </>
   )
 }
--- projects/form-master/src/__form__.js
+++ projects/form-master/src/__form__.js
@@ -9,4 +9,6 @@
  * 
  * 4. uncontrolled element: useRef to create a reference to the element and access value by using like: nameRef.current.value
  * 
+ * 5. use hook to handle states and submit and error. example: react hook form
+ * 
  */
