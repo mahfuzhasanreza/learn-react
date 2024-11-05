4849cfe6ab747ec0c38b8aec0c676d8c5f727bef
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-11-05 12:33:37 +0600

    added Context API to avoid Prop drilling

--- projects/form-master/src/components/Aunty/Aunty.jsx
+++ projects/form-master/src/components/Aunty/Aunty.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import Cousin from "../Cousin/Cousin";
+import { MoneyContext } from "../Grandpa/Grandpa";
 
 const Aunty = () => {
+    const [moeny, setMoney] = useContext(MoneyContext);
     return (
         <div>
             <h2>Aunty</h2>
@@ -8,6 +11,8 @@ const Aunty = () => {
                 <Cousin name={'Reza'}></Cousin>
                 <Cousin name={'Maha'}></Cousin>
             </section>
+            <p>Money: {moeny}</p>
+            <button onClick={() => setMoney(moeny + 1000)}>Add 1000 tk</button>
         </div>
     );
 };
