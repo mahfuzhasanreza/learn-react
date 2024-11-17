f6eafb011afc5846a2da91eb160b6b8a4b65a68a
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-11-17 19:43:38 +0600

    added google signup popup

--- simple-firebase/src/components/Login/Login.jsx
+++ simple-firebase/src/components/Login/Login.jsx
@@ -1,11 +1,19 @@
-import { GoogleAuthProvider } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import auth from "../../firebase/firebase.init";
 
 const Login = () => {
 
     const provider = new GoogleAuthProvider();
 
     const handleGoogleSignIn = () => {
-        console.log('hi');
+        console.log(auth, provider);
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                console.log(result);
+            })
+            .catch(error=>{
+                console.log('ERROR', error);
+            })
     }
 
     return (
