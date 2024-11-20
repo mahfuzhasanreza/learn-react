331e8c946dc4365530ac13d430967440bbdb5386
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-11-21 04:00:27 +0600

    validate email address and forget password

--- email-password-auth/src/components/Register/Register.jsx
+++ email-password-auth/src/components/Register/Register.jsx
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, sendEmailVerification } from 'firebase/auth';
 import React, { useState } from 'react';
 import { auth } from '../../firebase.init';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
@@ -45,6 +45,12 @@ const Register = () => {
             .then(result => {
                 console.log(result.user);
                 setSuccess(true);
+
+                // send verification email address
+                sendEmailVerification(auth.currentUser)
+                .then(()=>{
+                    console.log('verification email sent');
+                })
             })
             .catch(error => {
                 console.log('ERROR', error);
