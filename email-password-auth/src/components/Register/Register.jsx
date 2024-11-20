9a7f57b0623f234db9789965fa43aea5c68c5142
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-11-20 20:01:07 +0600

    added toggle show password and accept terms

--- email-password-auth/src/components/Register/Register.jsx
+++ email-password-auth/src/components/Register/Register.jsx
@@ -1,29 +1,39 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import React, { useState } from 'react';
 import { auth } from '../../firebase.init';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const Register = () => {
     const [success, setSuccess] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleRegister = (event) => {
         event.preventDefault();
         const email = event.target.email.value;
         const password = event.target.password.value;
-        console.log(email, password);
+
+        const terms = event.target.terms.checked;
+
+        console.log(email, password, terms);
 
         // reset error and status
         setErrorMessage('');
         setSuccess(false);
 
-        if(password.length < 6){
+        if(!terms){
+            setErrorMessage('Please accept our terms and condition');
+            return;
+        }
+
+        if (password.length < 6) {
             setErrorMessage('Password should be 6 character or longer');
             return;
         }
 
         const passwordRegex = /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
 
-        if(!passwordRegex.test(password)){
+        if (!passwordRegex.test(password)) {
             setErrorMessage('At least one uppercase, one lowercase, one number, one special character');
             return;
         }
@@ -57,7 +67,7 @@ const Register = () => {
                         <path
                             d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
                     </svg>
-                    <input type="email" name='email' className="grow" placeholder="Email" />
+                    <input type="email" name='email' className="grow" placeholder="Email" required />
                 </label>
 
                 <label className="input input-bordered flex items-center gap-2">
@@ -71,8 +81,27 @@ const Register = () => {
                             d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
                             clipRule="evenodd" />
                     </svg>
-                    <input type="password" name='password' className="grow" placeholder='Password' />
+                    <input
+                        type={showPassword ? 'text' : 'password'}
+                        name='password'
+                        className="grow"
+                        placeholder='Password' required />
+
+                    <button
+                        onClick={() => setShowPassword(!showPassword)}
+                        className='btn btn-xs'>
+                        {
+                            showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
+                        }
+                    </button>
                 </label>
+
+                <div className="form-control">
+                    <label className="label justify-start cursor-pointer">
+                        <input type="checkbox" name='terms' className="checkbox" />
+                        <span className="label-text ml-2">Accept our terms and condition</span>
+                    </label>
+                </div>
                 <button className='btn btn-accent btn-wide my-8'>Login</button>
             </form>
             {
