1769bae074cda6104877f39eaba3f6fc3a835701
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-11-21 11:18:51 +0600

    initialize project setup

---
+++ auth-integration-and-private-route/src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAO0uO4yCGGaTTwLemLcRa5x99b9CA0sSA",
+  authDomain: "auth-and-private-route-2f610.firebaseapp.com",
+  projectId: "auth-and-private-route-2f610",
+  storageBucket: "auth-and-private-route-2f610.firebasestorage.app",
+  messagingSenderId: "24331881354",
+  appId: "1:24331881354:web:cb199e1841cb2dc2ac9673"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
